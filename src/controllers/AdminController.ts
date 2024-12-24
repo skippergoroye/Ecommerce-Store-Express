a05201c25a0ad6244bc500ee361b83a378ab9a14
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-12-24 15:20:50 +0100

    feat: getting all vendor added

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import { CreateVandorInput } from '../dto';
 import { Vendor } from '../models';
+import { GeneratePassword, GenerateSalt } from '../utility/PasswordUtility';
 
 
 
@@ -12,13 +13,17 @@ export const CreateVendor = async (
     try {
         const { name, address, pincode, foodType, email, password, ownerName, phone }  = <CreateVandorInput>req.body;
 
-
         const CheckExistingVendor = await Vendor.findOne({email : email})
-
 
         if(CheckExistingVendor !== null){ 
           return res.json({message: "A Vendor already exists with this email"})
         }
+
+
+
+        //generate password and salt
+        const salt = await GenerateSalt();
+        const userPassword = await GeneratePassword(password, salt);
 
 
         const createdVandor =  await Vendor.create({
@@ -27,8 +32,8 @@ export const CreateVendor = async (
             pincode: pincode,
             foodType: foodType,
             email: email,
-            password: password,
-            salt: 'jjjjjj kkkkkkkk',
+            password: userPassword,
+            salt: salt,
             ownerName: ownerName,
             phone: phone,
             rating: 0,
@@ -42,12 +47,32 @@ export const CreateVendor = async (
 
   
     } catch (error) {
+      console.log(error)
       next(error);
     }
   };
 
 
 export const GetVendors = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+
+
+
+    const vendors = await Vendor.find()
+
+
+    if(vendors !== null){
+      return res.json(vendors)
+    }
+
+
+    return res.json({ "message": "vendor data not available"})
+    
+  } catch (error) {
+    console.log(error)
+    next(error);
+    
+  }
     
 }
 
